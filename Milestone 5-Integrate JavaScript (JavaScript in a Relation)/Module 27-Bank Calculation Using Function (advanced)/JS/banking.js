fa31b2148aef5b721f0577401dc5798902778ba1
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2022-02-12 09:46:19 +0600

    Add files via upload

--- Milestone 5-Integrate JavaScript (JavaScript in a Relation)/Module 27-Bank Calculation Using Function (advanced)/JS/banking.js
+++ Milestone 5-Integrate JavaScript (JavaScript in a Relation)/Module 27-Bank Calculation Using Function (advanced)/JS/banking.js
@@ -1,9 +1,9 @@
-// function doubleIt(num) {
-//   const result = num * 2;
-//   return result;
-// }
-// const fiveDouble = doubleIt(5);
-// const severDouble = doubleIt(7);
+function doubleIt(num) {
+  const result = num * 2;
+  return result;
+}
+const fiveDouble = doubleIt(5);
+const severDouble = doubleIt(7);
 
 function getInputValue(inputId) {
   debugger;
@@ -43,8 +43,28 @@ function updateBalance(amount, isAdd) {
   }
 }
 
-document.getElementById("deposit-button").addEventListener("click", function () {
+document
+  .getElementById("deposit-button")
+  .addEventListener("click", function () {
+    /* 
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText); */
 
+    // get and update deposit total
+    /*  
+    const depositTotal = document.getElementById('deposit-total');
+     const depositTotalText = depositTotal.innerText;
+     const previousDepositTotal = parseFloat(depositTotalText);
+ 
+     depositTotal.innerText = previousDepositTotal + depositAmount; */
+
+    // update balance
+    /* 
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previousBalanceTotal + depositAmount; */
     const depositAmount = getInputValue("deposit-input");
     if (depositAmount > 0) {
       updateTotalField("deposit-total", depositAmount);
@@ -53,7 +73,28 @@ document.getElementById("deposit-button").addEventListener("click", function () 
   });
 
 // handle withdraw button
-document.getElementById("withdraw-button").addEventListener("click", function () {
+document
+  .getElementById("withdraw-button")
+  .addEventListener("click", function () {
+    /*     
+    const withdrawInput = document.getElementById('withdraw-input');
+        const withdrawAmountText = withdrawInput.value;
+        const withdrawAmount = parseFloat(withdrawAmountText); */
+
+    // get and update withdraw total
+    /* 
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawTotalText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalText);
+    withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount; */
+
+    // update balance after withdraw
+    /* 
+        const balanceTotal = document.getElementById('balance-total');
+        const balanceTotalText = balanceTotal.innerText;
+        const previousBalanceTotal = parseFloat(balanceTotalText);
+    
+        balanceTotal.innerText = previousBalanceTotal - withdrawAmount; */
 
     const withdrawAmount = getInputValue("withdraw-input");
     const currentBalance = getCurrentBalance();
