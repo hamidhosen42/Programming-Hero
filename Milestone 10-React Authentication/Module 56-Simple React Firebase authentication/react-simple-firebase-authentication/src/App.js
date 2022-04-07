b04a57cb53be6e78775f26cdf0e5dafab4df64db
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2022-04-07 11:01:59 +0600

    Add files via upload

---
+++ Milestone 10-React Authentication/Module 56-Simple React Firebase authentication/react-simple-firebase-authentication/src/App.js
@@ -0,0 +1,66 @@
+import "./App.css";
+import app from "./firebase.init";
+import { getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+import { useState } from "react";
+import { GithubAuthProvider } from "firebase/auth";
+
+const auth = getAuth(app);
+
+function App() {
+  const [user, setUser] = useState([]);
+
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth,googleProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleGithubSignIn=()=>{
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  return (
+    <div className="App">
+      {user.uid ? (
+        <button onClick={handleGoogleSignOut}>Google Sign Out</button>
+      ) : (
+        <>
+          <button onClick={handleGoogleSignIn}>Google Sign In</button>
+          <button onClick={handleGithubSignIn}>Github Sign In</button>
+        </>
+      )}
+      <h2>Name : {user.displayName}</h2>
+      <h3>Gmail : {user.email}</h3>
+      <img src={user.photoURL} alt="" srcset="" />
+    </div>
+  );
+}
+
+export default App;
