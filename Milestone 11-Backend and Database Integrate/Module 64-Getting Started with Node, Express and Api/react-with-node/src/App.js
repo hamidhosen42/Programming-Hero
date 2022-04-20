67ed3ea686fdbbeead9e090eb89c8e7cfd5b50a4
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2022-04-21 01:48:32 +0600

    Add files via upload

---
+++ Milestone 11-Backend and Database Integrate/Module 64-Getting Started with Node, Express and Api/react-with-node/src/App.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [users,setUsers]=useState([]);
+
+  useEffect(()=>{
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  },[]);
+
+  const handleAddUser=(event)=>{
+    event.preventDefault();
+
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+
+    // console.log(name,email);
+    const user = { name, email };
+
+    // post data to server
+      fetch('http://localhost:5000/user', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+        console.log(data);
+      })
+  }
+
+  return (
+    <div className="App">
+      <h1>My Own data: {users.length}</h1>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="Name" required />
+        <input type="text" name="email" placeholder="Email" required />
+        <input type="submit" value="Add User" />
+      </form>
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            id:{user.id}
+            name:{user.name}
+            gmail:{user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
